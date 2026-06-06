3a4fd33e6a18deecceb0d3e21835d94b1d74cd7f
jiasr <jiasirui3@163.com>
2026-06-06 23:13:31 +0800

    update

--- src/api/goods.js
+++ src/api/goods.js
@@ -2,15 +2,15 @@ import axios from '~/axios'
 
 // 获取商品列表
 export function getGoodsList(params) {
-    return axios.get("/v1/goods/list", { params })
+    return axios.get("/mall/v1/goods/list", { params })
 }
 
 // 获取商品详情
 export function getGoodsDetail(spuId) {
-    return axios.get("/v1/goods/detail", { params: { spuId } })
+    return axios.get("/mall/v1/goods/detail", { params: { spuId } })
 }
 
 // 新增商品
 export function addGoods(data) {
-    return axios.post("/v1/goods/admin/goods/add", data)
+    return axios.post("/mall/v1/goods/admin/goods/add", data)
 }
