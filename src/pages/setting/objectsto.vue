232ab124503a86779a54c7d6b1e58747d8595970
jiasr <jiasirui3@163.com>
2026-06-12 22:59:50 +0800

    style: 存储配置页改为单列 label-width=150px

--- src/pages/setting/objectsto.vue
+++ src/pages/setting/objectsto.vue
@@ -16,34 +16,28 @@
                 </template>
             </el-alert>
 
-            <el-form :model="form" label-width="130px" class="storage-form">
-                <el-row :gutter="40">
-                    <el-col :xs="24" :md="12">
-                        <el-form-item label="Endpoint">
-                            <el-input v-model="form.endpoint" placeholder="http://82.156.225.136:9000" />
-                            <span class="form-tip">存储服务地址</span>
-                        </el-form-item>
-                        <el-form-item label="Bucket 名称">
-                            <el-input v-model="form.bucket_name" placeholder="mall-images1" />
-                        </el-form-item>
-                        <el-form-item label="Region">
-                            <el-input v-model="form.region" placeholder="us-east-1" />
-                            <span class="form-tip">MinIO / COS / OSS 传 us-east-1，AWS S3 填真实 Region</span>
-                        </el-form-item>
-                        <el-form-item label="公网地址">
-                            <el-input v-model="form.public_endpoint" placeholder="http://82.156.225.136:9000" />
-                            <span class="form-tip">前端图片访问地址</span>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :xs="24" :md="12">
-                        <el-form-item label="AccessKey ID">
-                            <el-input v-model="form.access_key" placeholder="admin" />
-                        </el-form-item>
-                        <el-form-item label="AccessKey Secret">
-                            <el-input v-model="form.secret_key" placeholder="password123" show-password />
-                        </el-form-item>
-                    </el-col>
-                </el-row>
+            <el-form :model="form" label-width="150px" class="storage-form">
+                <el-form-item label="Endpoint">
+                    <el-input v-model="form.endpoint" placeholder="http://82.156.225.136:9000" />
+                    <span class="form-tip">存储服务地址</span>
+                </el-form-item>
+                <el-form-item label="Bucket 名称">
+                    <el-input v-model="form.bucket_name" placeholder="mall-images1" />
+                </el-form-item>
+                <el-form-item label="Region">
+                    <el-input v-model="form.region" placeholder="us-east-1" />
+                    <span class="form-tip">MinIO / COS / OSS 传 us-east-1，AWS S3 填真实 Region</span>
+                </el-form-item>
+                <el-form-item label="公网地址">
+                    <el-input v-model="form.public_endpoint" placeholder="http://82.156.225.136:9000" />
+                    <span class="form-tip">前端图片访问地址</span>
+                </el-form-item>
+                <el-form-item label="AccessKey ID">
+                    <el-input v-model="form.access_key" placeholder="admin" />
+                </el-form-item>
+                <el-form-item label="AccessKey Secret">
+                    <el-input v-model="form.secret_key" placeholder="password123" show-password />
+                </el-form-item>
                 <el-form-item>
                     <el-button type="primary" :loading="saving" @click="handleSave">保存配置</el-button>
                     <el-button @click="handleReset">恢复默认</el-button>
@@ -132,7 +126,7 @@ onActivated(() => { loadSetting() })
 .mb-5 { margin-bottom: 20px; }
 
 .storage-form {
-    max-width: 100%;
+    max-width: 650px;
 }
 
 .card-header-row {
